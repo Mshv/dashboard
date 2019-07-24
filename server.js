0f6d4687b3aca1b8ad20a370c94424d872da24db
mehdi shahveri <mehdi.shahverdi@axiata.com>
2019-07-24 12:17:40 +0800

    add corse config

--- server.js
+++ server.js
@@ -3,7 +3,23 @@ const path = require('path'),
    webpack = require('webpack'),
    webpackConfig = require('./webpack.config.js'),
    app = express(),
-   port = process.env.PORT || 2000;
+   port = process.env.PORT || 8080,
+   cors = require('cors');
+// app.use(cors());
+//enables cors
+app.use(cors({
+   'allowedHeaders': ['sessionId', 'Content-Type'],
+   'exposedHeaders': ['sessionId'],
+   'origin': '*',
+   'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
+   'preflightContinue': false
+ }));
+app.use(function(req, res, next) {
+   res.header("Access-Control-Allow-Origin", "*");
+   res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
+   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+   next();
+});
 app.listen(port, () => { console.log(`App is listening on port ${port}`) });
 app.get('/', (req, res) => {
    res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
